315e119444e4ee7296b9dddf9389ed92d3d99fa5
Wojciech Koczwara <kojciech.woczwara@gmail.com>
2017-11-06 00:50:51 +0100

    poprawiki

--- src/main/java/modulZarzadzajacy/ModulZarzadzajacy.java
+++ src/main/java/modulZarzadzajacy/ModulZarzadzajacy.java
@@ -1,19 +1,11 @@
 package modulZarzadzajacy;
 
-import modulZarzadzajacy.mvc.AppStart;
 import modulZarzadzajacy.mvc.controller.ControllerEkranGlowny;
 import modulZarzadzajacy.mvc.view.ViewEkranGlowny;
 
 public class ModulZarzadzajacy {
     public static void main(String[] args) {
 
-     /*   try {
-            AppStart aplikacja = new AppStart();
-            aplikacja.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    */
         ControllerEkranGlowny aplikacja = new ControllerEkranGlowny(new ViewEkranGlowny());
         aplikacja.Start();
     }
